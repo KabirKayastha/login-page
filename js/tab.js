a56b88f7d4bb8a00ffb686d3cc977bac319788e5
KabirKayastha <kayas.kabie@gmail.com>
2018-11-20 00:26:41 +0545

    developed lofin form; JavaScript yet to be done

---
+++ js/tab.js
@@ -0,0 +1,18 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex,opacityAmount) {
+	tabButtons.forEach(function(node){
+		node.style.opacity="";
+		node.style.color="";
+	});
+	tabButtons[panelIndex].style.opacity=opacityAmount;
+	tabButtons[panelIndex].style.color="white";
+	tabPanels.forEach(function(node){
+		node.style.display="NONE"; 
+	});
+	tabPanels[panelIndex].style.display="block";
+	//tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,1);
+
